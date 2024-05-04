bdf38001d0aaa06b4470c36890e790aa3fc1ca8f
Don Han <plestoon@gmail.com>
2024-05-04 14:45:46 +0800

    configure google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'ffi/frb_generated.dart';
@@ -7,6 +9,13 @@ import 'tls/tls_view.dart';
 
 void main() {
   RustLib.init();
+
+  GoogleFonts.config.allowRuntimeFetching = false;
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/LICENSE.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(const App());
 }
 
@@ -23,8 +32,8 @@ class App extends StatelessWidget {
         visualDensity: const VisualDensity(
             horizontal: VisualDensity.minimumDensity,
             vertical: VisualDensity.minimumDensity),
-        fontFamily: GoogleFonts.aBeeZee().fontFamily,
-        textTheme: GoogleFonts.aBeeZeeTextTheme(),
+        fontFamily: GoogleFonts.roboto().fontFamily,
+        textTheme: GoogleFonts.robotoTextTheme(),
       ),
       darkTheme: FlexThemeData.dark(
         scheme: FlexScheme.brandBlue,
@@ -32,8 +41,8 @@ class App extends StatelessWidget {
         visualDensity: const VisualDensity(
             horizontal: VisualDensity.minimumDensity,
             vertical: VisualDensity.minimumDensity),
-        fontFamily: GoogleFonts.aBeeZee().fontFamily,
-        textTheme: GoogleFonts.aBeeZeeTextTheme(),
+        fontFamily: GoogleFonts.roboto().fontFamily,
+        textTheme: GoogleFonts.robotoTextTheme(),
         scaffoldBackground: const Color.fromARGB(255, 40, 44, 51),
       ),
       themeMode: ThemeMode.system,
